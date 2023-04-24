8b777e23b1eea9c6c9ba5d139884a1d143974cd8
AlexandruTudorishere <128329301+AlexandruTudorishere@users.noreply.github.com>
2023-04-24 03:06:36 +0300

    Update sql_connection.py

--- Store_Management_System/Backend/sql_connection.py
+++ Store_Management_System/Backend/sql_connection.py
@@ -5,7 +5,7 @@ __cnx = None   ### check for multiple connections
 def get_sql_connection():
     global __cnx
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='Myrysen7',
-                                  host='127.0.0.1',
-                                  database='gs')
+        __cnx = mysql.connector.connect(user='root', password='Password',
+                                  host='.........',
+                                  database='.....')
     return __cnx
